262fac12986ea68ade93c1eae5b9ae4a557858ab
Patrick Uiterwijk <puiterwijk@redhat.com>
2018-06-20 15:10:13 +0200

    Add wsgi.py to use gunicorn
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
-""" app.py
+""" wsgi.py
 
 This is a small wrapper for openshift deployments.
 Author:     Patrick Uiterwijk <puiterwijk@redhat.com>
 License:    GPLv2+
 """
 
-from discourse2fedmsg import app
-app.run(host='0.0.0.0', port=8080)
+from discourse2fedmsg import app as application
