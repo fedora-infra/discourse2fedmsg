d464c0a7713e2f9106fb41211188de2bf391be8f
Ryan Lerch <rlerch@redhat.com>
2021-11-04 11:05:15 +1000

    remove supporting files, leave code and tests
    remove all the old project structure, as moving to a new one based
    generated with https://github.com/fedora-infra/cookiecutter-flask-webapp
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-""" wsgi.py
-
-This is a small wrapper for openshift deployments.
-Author:     Patrick Uiterwijk <puiterwijk@redhat.com>
-License:    GPLv2+
-"""
-
-from discourse2fedmsg import app as application
