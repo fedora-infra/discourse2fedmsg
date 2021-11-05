b20866b55a2e9568fa8f2e4bdbaa366ff67a81cc
Ryan Lerch <rlerch@redhat.com>
2021-11-05 14:45:28 +1000

    move and migrate the old tests
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -1,50 +1,10 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2021  Red Hat, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-"""
-Tests for discourse2fedmsg.
-"""
 import hashlib
 import hmac
 import json
 import unittest.mock as mock
 
 import pytest
-import os
-
-import discourse2fedmsg as d2f
-
-
-@pytest.fixture
-def app():
-    """
-    Return the flask client.
-    """
-    return d2f.app.test_client()
-
-
-def test_index(app):
-    """
-    Test for index function.
-    """
-    rv = app.get("/")
-
-    assert rv.status_code == 200
-    assert b"Source:  https://pagure.io/discourse2fedmsg" in rv.data
+from flask import current_app
 
 
 @mock.patch("fedmsg.publish")
