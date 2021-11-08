5eaa4e9f654ad5290f920e85b40a04fa0d94e31e
Ryan Lerch <rlerch@redhat.com>
2021-11-08 14:04:33 +1000

    update tests so they all work
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- discourse2fedmsg/defaults.py
+++ discourse2fedmsg/defaults.py
@@ -4,6 +4,7 @@ TEMPLATES_AUTO_RELOAD = False
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
 
+
 HEALTHZ = {
     "live": "discourse2fedmsg.utils.healthz.liveness",
     "ready": "discourse2fedmsg.utils.healthz.readiness",
