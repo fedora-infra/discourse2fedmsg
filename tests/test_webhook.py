1709147180bfd2d6fb7911597917ed7d7abb9dea
Ryan Lerch <rlerch@redhat.com>
2021-11-08 14:04:33 +1000

    update tests so they all work
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -3,31 +3,33 @@ import hmac
 import json
 import unittest.mock as mock
 
+from fedora_messaging import api, testing
 
-@mock.patch("fedmsg.publish")
-def test_webhook(mock_publish, app):
-    data = {"test_data": "data"}
-    calced_sig = hmac.new(
-        d2f.secret.encode(), json.dumps(data).encode(), hashlib.sha256
-    ).hexdigest()
-    rv = app.post(
-        "/webhook",
-        data=json.dumps(data),
-        headers={
-            "X-Discourse-Event-Signature": "sha256=" + calced_sig,
-            "X-Discourse-Event-Type": "test_event",
-        },
-    )
+def calc_sig(client, data):
+    return hmac.new(client.config["DISCOURSE2FEDMSG_SECRET"].encode(), json.dumps(data).encode(), hashlib.sha256).hexdigest()
+
+
+def test_webhook(app, client):
+    data = {"ping": "OK"}
+    headers={
+            "X-Discourse-Event-Signature": f"sha256={calc_sig(app, data)}",
+            "X-Discourse-Event-Type": "ping",
+            "X-Discourse-Event": "ping",
+    }
+    with testing.mock_sends(api.Message(topic="discourse.ping.ping", body=data)):
+        rv = client.post(
+            "/webhook",
+            data=json.dumps(data),
+            headers=headers,
+        )
 
     assert rv.status_code == 200
     assert b"Everything is 200 OK" in rv.data
 
-    mock_publish.assert_called_with(modname="discourse", topic="test_event", msg=data)
 
-
-def test_webhook_missing_header(app):
+def test_webhook_missing_header(client):
     data = {"test_data": "data"}
-    rv = app.post(
+    rv = client.post(
         "/webhook",
         data=json.dumps(data),
     )
@@ -36,16 +38,13 @@ def test_webhook_missing_header(app):
     assert b"No X-Discourse-Event-Signature found on request." in rv.data
 
 
-def test_webhook_wrong_hash(app):
+def test_webhook_wrong_hash(app, client):
     data = {"test_data": "data"}
-    calced_sig = hmac.new(
-        d2f.secret.encode(), json.dumps(data).encode(), hashlib.sha256
-    ).hexdigest()
-    rv = app.post(
+    rv = client.post(
         "/webhook",
         data=json.dumps(data),
         headers={
-            "X-Discourse-Event-Signature": calced_sig,
+            "X-Discourse-Event-Signature": {calc_sig(app, data)},
             "X-Discourse-Event-Type": "test_event",
         },
     )
@@ -54,9 +53,9 @@ def test_webhook_wrong_hash(app):
     assert b"No sha256 prefix found." in rv.data
 
 
-def test_webhook_not_valid_sig(app):
+def test_webhook_not_valid_sig(client):
     data = {"test_data": "data"}
-    rv = app.post(
+    rv = client.post(
         "/webhook",
         data=json.dumps(data),
         headers={
